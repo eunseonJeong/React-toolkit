86c179aa061915f8fc8ed92a451718048c2e11a9
eunseon Jeong <suni980215@gmail.com>
2023-03-07 11:01:02 +0900

    CRUD

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { newTodo, doneState, deleteTodo, editState, fixTodo, removeTodo } from "./reducer/modules/TodoModule";
+import "./App.css";
+
+function App() {
+  const todoList = useSelector((state) => state.todo);
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [fixTitle, setFixTitle] = useState("");
+  const [fixText, setFixText] = useState("");
+
+  const titleTodo = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const textTodo = (e) => {
+    setText(e.target.value);
+  };
+
+  const fixTitleTodo = (e) => {
+    setFixTitle(e.target.value);
+  };
+
+  const fixTextTodo = (e) => {
+    setFixText(e.target.value);
+  };
+
+  return (
+    <>
+      <header> TodoList </header>
+      <form>
+        <label htmlFor="">제목 : </label>
+        <input type="text" onChange={titleTodo}/>
+        <label htmlFor="">내용 : </label>
+        <input type="text" onChange={textTodo}/>
+        <button onClick={(e)=>{
+          e.preventDefault();
+          dispatch(newTodo({title, text}))
+        }}>작성</button>
+      </form>
+
+      <hr />
+
+        <div>
+          <h2>해야 할 일</h2>
+          {todoList.map((item)=>
+            !item.isDone ?
+              <div key={item.id}>
+                {/* <h3>{item.title}</h3>
+                <p>{item.text}</p> */}
+                {item.edit ? 
+                  <div>
+                    <input value={fixTitle} onChange={fixTitleTodo}/>
+                    <input value={fixText} onChange={fixTextTodo}/>
+                    <button onClick={()=>{
+                      dispatch(fixTodo([item.id, fixTitle, fixText]))
+                    }}>수정완료</button>
+                      <button onClick={()=>{
+                        dispatch(doneState(item.id),removeTodo(item.id))
+                      }}>완료</button>
+                      <button onClick={()=>{
+                        dispatch(deleteTodo(item.id))
+                      }}>삭제</button>
+                  </div>
+                  : 
+                  <div>
+                    <h3>{item.title}</h3>
+                    <p>{item.text}</p>
+                    <button onClick={()=>{
+                      dispatch(editState(item.id))
+                      setFixTitle(item.title)
+                      setFixText(item.text)
+                    }}>수정</button>
+                    <button onClick={()=>{
+                      dispatch(doneState(item.id))
+                    }}>완료</button>
+                    <button onClick={()=>{
+                      dispatch(deleteTodo(item.id))
+                    }}>삭제</button>
+                  </div>
+                }
+              </div>
+            : null
+          )}
+        </div>
+
+        <hr />
+        <div>
+          <div>
+            <h2>끝난 일</h2>
+          {todoList.map((item)=>
+            item.isDone ?
+              <div key={item.id}>
+                <h3>{item.title}</h3>
+                <p>{item.text}</p>
+                <button onClick={()=>{
+                  dispatch(doneState(item.id))
+                }}>취소</button>
+                <button onClick={()=>{
+                  dispatch(deleteTodo(item.id))
+                }}>삭제</button>
+              </div>
+            : null
+          )}
+          </div>
+         </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/reducer/modules/TodoModule.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        newTodo: (state, {payload}) => {
+            state.push({
+                id: state.length + 1,
+                title: payload.title,
+                text: payload.text,
+                isDone: false,
+                edit: false,
+            })
+        },
+
+        doneState: (state, {payload}) => {
+            return state.map((item) => item.id === payload ? {...item, isDone: !item.isDone} : item)
+        },
+
+        deleteTodo: (state, {payload}) => {
+            return state.filter((item)=> payload !== item.id);
+        },
+
+        editState: (state, {payload}) => {
+            return state.map((item) => item.id === payload ? {...item, edit: !item.edit} : item);
+        },
+
+        fixTodo: (state, {payload}) => {
+            return state.map((item) => item.id === payload[0] ? {...item, title: payload[1], text: payload[2], edit: !item.edit} : item);
+        },
+        removeTodo: (state, {payload}) => {
+            state.push({
+                title:'',
+                text:'',
+                payload,
+            })
+        }
+    }
+})
+
+export const { newTodo, doneState, deleteTodo, editState, fixTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
